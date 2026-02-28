c8cb68e38f072d326cd2cd3f0bdfcc6247bb638a
Atul Singh <atul.singh2189@gmail.com>
2026-02-28 22:49:32 +0530

    Initial commit: Add Basics1.js

---
+++ Basics1.js
@@ -0,0 +1,37 @@
+console.log("Hello world")
+
+// these are the comments
+
+/*
+
+*/
+
+//var a =4
+
+// var  (let, const) ES6
+
+
+let b=4
+console.log(b)
+console.log(typeof(b))
+
+
+let c = 234.5
+console.log(c)
+console.log(typeof(c))
+
+
+let d ="Atul singh"
+console.log(typeof(d))
+
+
+let required =true
+console.log(typeof(required))
+
+let f = b+c
+console.log(f)
+
+// we cannot redeclare variable with let keyword but possible with var 
+
+
+console.log(!required)
